ca3a4d1543c9725e0dd06f228b245c969c32fa86
Gaurav B Kapoor <gauravbk20@gmail.com>
2026-09-07 22:43:59 -0500

    added word hello

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-  return "Welcome to Python flask world v2.0"
+def hello():
+  return "Hello Welcome to Python flask world v2.0"
 
 if __name == '__main__':
   app.run(host='0.0.0.0',port=8080)
