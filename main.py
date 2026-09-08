2bd53fdcb0cfed287e5dabcb715b5b9175f4507a
gauravbk20 <135913686+gauravbk20@users.noreply.github.com>
2026-09-07 21:32:13 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Welcome to Python flask world v2.0"
+
+if __name == '__main__':
+  app.run(host='0.0.0.0',port=8080)
