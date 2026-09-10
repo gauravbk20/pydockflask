4e569415d90e7e083f51e517cbcd34a643d90d01
Gaurav B Kapoor <gauravbk20@gmail.com>
2026-09-09 21:40:46 -0500

    revi1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Hello Welcome to Python flask world v2.0 got it !!"
+  return "Hello WC to Python flask world v2.0 got it !!"
 
 if __name == '__main__':
   app.run(host='0.0.0.0',port=8080)
